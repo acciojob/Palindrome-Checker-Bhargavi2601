5d78d3489e5a3f1a0082cfe3c44ae00eb85bd64e
Bhargavi Salivoju <117651365+Bhargavi2601@users.noreply.github.com>
2023-01-19 22:17:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 // complete the given function
 
 function palindrome(str){
+var removeChar=str.replace(/[^A-Z0-9]/ig, "").toLowerCase();
+	var checkPalindrome=removeChar.split('').reverse().join('');
 
+	
+	if(removeChar===checkPalindrome){
+		return=true;
+	}else{
+		return=false;
+	}
 }
 module.exports = palindrome
